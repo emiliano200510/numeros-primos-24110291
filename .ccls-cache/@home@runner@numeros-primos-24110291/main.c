604e2d1dc43cf4ff06759c28b6359238032f1856
emiliano200510 <160807793+emiliano200510@users.noreply.github.com>
2024-05-14 23:19:28 +0000

    segundo commit

--- .ccls-cache/@home@runner@numeros-primos-24110291/main.c
+++ .ccls-cache/@home@runner@numeros-primos-24110291/main.c
@@ -3,13 +3,21 @@
 
 int esPrimo(int num) {
     if (num <= 1) return 0; 
-    if (num <= 3) return 1; 
-
-  
     for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
-            return 0;
-        }
+        if (num % i == 0) return 0; 
     }
     return 1; 
+    
 }
+// segunda parte 
+    int main() {
+        int inicio, fin;
+
+
+        printf("Ingrese el inicio del rango: ");
+        scanf("%d", &inicio);
+        printf("Ingrese el final del rango: ");
+        scanf("%d", &fin);
+
+        printf("Los numeros primos en el rango [%d, %d] son:\n", inicio, fin);
+
